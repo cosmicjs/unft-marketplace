12fcae8f5e14aac46872e44e52eb610a5470d08c
Naira <nairagezhoyan@gmail.com>
2022-06-13 15:50:17 +0400

    remove skeleton

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,24 +1,20 @@
 import React from "react";
 import Image from 'next/image';
 import useDarkMode from "use-dark-mode";
-import Skeletons from '../Skeletons';
 
 const ImageApp = ({ className, src, srcDark, alt, size, objectFit="contain" }) => {
   const darkMode = useDarkMode(false);
 
   return (
     <div className={className} style={{ ...size, position: 'relative' }}>
-      {!src?.length ?
-        <Skeletons /> :
-        <Image
-          src={( darkMode.value && srcDark ) ? srcDark : src}
-          alt={alt}
-          layout="fill"
-          objectFit={objectFit}
-          placeholder="blur"
-          blurDataURL={`${src}?w=10`}
-        />
-      }
+      <Image
+        src={( darkMode.value && srcDark ) ? srcDark : src}
+        alt={alt}
+        layout="fill"
+        objectFit={objectFit}
+        placeholder="blur"
+        blurDataURL={`${src}?w=10`}
+      />
   </div>
   );
 };
